593256126dfac7449003ea2156318ed5a762864f
0mer-Ashraf-ML <omarashrafml001@gmail.com>
2025-04-25 15:11:28 +0500

    Ad spinner globally

---
+++ src/app/services/spinner.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({ providedIn: 'root' })
+export class SpinnerService {
+  private defaultConfig = {
+    bdColor: 'rgba(0, 0, 0, 0.8)',
+  };
+
+  constructor(private spinner: NgxSpinnerService) {}
+
+  show() {
+    this.spinner.show(undefined, this.defaultConfig);
+  }
+
+  hide() {
+    this.spinner.hide();
+  }
+}
