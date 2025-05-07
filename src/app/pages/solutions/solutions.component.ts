dca8491383fb2a7acd91d117692909ca90fe7c18
0mer-Ashraf-ML <omarashrafml001@gmail.com>
2025-05-07 19:16:49 +0500

    Update section

--- src/app/pages/solutions/solutions.component.ts
+++ src/app/pages/solutions/solutions.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../shared/components/hero/hero.component';
 import { B2bSalesComponent } from './components/b2b-sales/b2b-sales.component';
-import { ApplicationsComponent } from '../home/components/applications/applications.component';
+import { ApplicationsComponent } from '../solutions/components/applications/applications.component';
 import { CtaComponent } from '../shared/components/cta/cta.component';
 
 @Component({
